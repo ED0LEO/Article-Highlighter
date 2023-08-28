c99fe4bef192859eb9df6378f52a9348d7453878
ED0LEO <ed0leo@proton.me>
2023-08-28 02:08:11 +0200

    Coloring is now better adjusted to dark modes

--- content.js
+++ content.js
@@ -17,29 +17,17 @@ function applyHighlighting() {
             });
         } else if (document.body) {
             // Apply highlighting and styles
-            const colors = [
-                '#FFD699', // Light Orange
-                '#AED6F1', // Light Blue
-                '#D5DBDB', // Light Gray
-                '#D1F2EB', // Light Green
-                '#F5B7B1', // Light Pink
-                '#FAD7A0', // Light Yellow
-                '#A3E4D7', // Light Turquoise
-                '#F9E79F', // Light Pastel Yellow
-                '#F0B27A', // Light Apricot
-                '#BB8FCE'  // Light Lavender
-            ];
-
-            // Apply highlighting to paragraphs
             const paragraphs = document.querySelectorAll('p');
             paragraphs.forEach((paragraph) => {
                 if (paragraph.textContent.trim() !== '') {
-                    const color = colors[Math.floor(Math.random() * colors.length)];
-                    paragraph.style.backgroundColor = color;
+                    const computedStyle = getComputedStyle(paragraph);
+                    const textColor = computedStyle.color;
+                    const backgroundColor = getAdjustedBackgroundColor(textColor);
+
+                    paragraph.style.backgroundColor = backgroundColor;
                     if (roundedCorners) {
                         paragraph.style.borderRadius = '10px';
-                    }
-                    else {
+                    } else {
                         paragraph.style.borderRadius = '';
                     }
                     paragraph.style.padding = '4px';
@@ -47,6 +35,49 @@ function applyHighlighting() {
             });
         }
     });
+}
+
+function getAdjustedBackgroundColor(textColor) {
+    const rgb = textColor.match(/\d+/g);
+    if (rgb) {
+        const sum = rgb.reduce((acc, val) => acc + parseInt(val), 0);
+        const average = sum / 3;
+        const medium = average >= 64 && average <= 192;
+
+        const transparency = medium ? 0.3 : 0.5; // Adjust transparency based on text color darkness
+
+        const colors = [
+            'rgba(255, 214, 153, ' + transparency + ')', // Light Orange with adjusted transparency
+            'rgba(174, 214, 241, ' + transparency + ')', // Light Blue with adjusted transparency
+            'rgba(213, 219, 219, ' + transparency + ')', // Light Gray with adjusted transparency
+            'rgba(209, 242, 235, ' + transparency + ')', // Light Green with adjusted transparency
+            'rgba(245, 183, 177, ' + transparency + ')', // Light Pink with adjusted transparency
+            'rgba(250, 215, 160, ' + transparency + ')', // Light Yellow with adjusted transparency
+            'rgba(163, 228, 215, ' + transparency + ')', // Light Turquoise with adjusted transparency
+            'rgba(249, 231, 159, ' + transparency + ')', // Light Pastel Yellow with adjusted transparency
+            'rgba(240, 178, 122, ' + transparency + ')', // Light Apricot with adjusted transparency
+            'rgba(187, 143, 206, ' + transparency + ')'  // Light Lavender with adjusted transparency
+        ];
+
+        const randomIndex = Math.floor(Math.random() * colors.length);
+        return colors[randomIndex];
+    }
+
+    const colors = [
+        'rgba(255, 214, 153, 0.5)', // Light Orange with adjusted transparency
+        'rgba(174, 214, 241, 0.5)', // Light Blue with adjusted transparency
+        'rgba(213, 219, 219, 0.5)', // Light Gray with adjusted transparency
+        'rgba(209, 242, 235, 0.5)', // Light Green with adjusted transparency
+        'rgba(245, 183, 177, 0.5)', // Light Pink with adjusted transparency
+        'rgba(250, 215, 160, 0.5)', // Light Yellow with adjusted transparency
+        'rgba(163, 228, 215, 0.5)', // Light Turquoise with adjusted transparency
+        'rgba(249, 231, 159, 0.5)', // Light Pastel Yellow with adjusted transparency
+        'rgba(240, 178, 122, 0.5)', // Light Apricot with adjusted transparency
+        'rgba(187, 143, 206, 0.5)',  // Light Lavender with adjusted transparency
+    ];
+
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
 }
 
 // Apply highlighting when the content script is executed
